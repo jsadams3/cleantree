7dd2e9b352aa3b538bf5f03ceaded971379493dc
Bronwyn Dunn <bronwynleigh@berkeley.edu>
2021-01-23 19:24:43 -0600

    update form values

--- src/components/Aquaponic/Form.js
+++ src/components/Aquaponic/Form.js
@@ -60,15 +60,15 @@ const AquaponicForm = () => {
               <Select onChange={handleChange}>
                 <Option value="Northeast">Northeast</Option>
                 <Option value="Southeast">Southeast</Option>
-                <Option value="Upper Midwest (East North Central)">
+                <Option value="upper_midwest_east_north_central">
                   Upper Midwest (East North Central)
                 </Option>
-                <Option value="Ohio Valley (Central)">
+                <Option value="ohio_valley_central">
                   Ohio Valley (Central)
                 </Option>
                 <Option value="South">South</Option>
                 <Option value="Southwest">Southwest</Option>
-                <Option value="Northern Rockies and Plains (West North Central)">
+                <Option value="northern_rockies_and_plains_west_north_central">
                   Northern Rockies and Plains (West North Central)
                 </Option>
                 <Option value="West">West</Option>
