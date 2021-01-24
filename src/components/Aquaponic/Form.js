9586d725ab0b2b036c9381ba155b7b1b721a5de0
Hunter Houston <hmhouston7@gmail.com>
2021-01-24 12:57:55 -0600

    remove antd form

--- src/components/Aquaponic/Form.js
+++ src/components/Aquaponic/Form.js
@@ -73,128 +73,17 @@ const AquaponicForm = () => {
   };
 
   return (
-    <div className="aquaponic-form-wrapper">
-      <form
-        name={formName}
-        data-netlify="true"
-        data-netlify-honeypot="bot-field"
-        hidden
-      >
-        <input type="text" name="region" />
-        <input type="text" name="zipcode" />
-      </form>
-
-      <Form
-        name="basic"
-        method="POST"
-        onFinish={handleSubmit}
-        layout="vertical"
-        {...layout}
-        justify="center"
-      >
-        <Row justify="center">
-          <Form.Item
-            label="Don't fill this out"
-            className={`hidden`}
-            style={{ display: `none` }}
-            name="bot-field"
-          >
-            <Input type={`hidden`} />
-          </Form.Item>
-
-          <Col span={12}>
-            <Form.Item
-              label="1. Enter your zip code"
-              name="zipcode"
-              className="label"
-            >
-              <Input />
-            </Form.Item>
-          </Col>
-
-          <Col span={12}>
-            <Form.Item
-              name="region"
-              label="4. Select your region"
-              className="label"
-            >
-              <Select onChange={handleChange}>
-                <Option value="Northeast">Northeast</Option>
-                <Option value="Southeast">Southeast</Option>
-                <Option value="upper_midwest_east_north_central">
-                  Upper Midwest (East North Central)
-                </Option>
-                <Option value="ohio_valley_central">
-                  Ohio Valley (Central)
-                </Option>
-                <Option value="South">South</Option>
-                <Option value="Southwest">Southwest</Option>
-                <Option value="northern_rockies_and_plains_west_north_central">
-                  Northern Rockies and Plains (West North Central)
-                </Option>
-                <Option value="West">West</Option>
-                <Option value="Northwest">Northwest</Option>
-              </Select>
-            </Form.Item>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={12}>
-            <Form.Item
-              label="2. What is your DIY experience?"
-              name="experience"
-              className="label"
-            >
-              <Slider min={0} max={10} />
-            </Form.Item>
-          </Col>
-          <Col span={12}>
-            <Form.Item label="5. Enter your sector" className="label">
-              <Select>
-                <Option value="Residential">Residential</Option>
-                <Option value="Commercial">Commercial</Option>
-                <Option value="Industrial">Industrial</Option>
-              </Select>
-            </Form.Item>
-          </Col>
-        </Row>
-
-        <Col span={12}>
-          <Form.Item label="3. What is your budget?" className="label">
-            <Select>
-              <Option value="0">$0-49,00</Option>
-              <Option value="1">$5000-99,000</Option>
-              <Option value="2">$10,000-24,000</Option>
-            </Select>
-          </Form.Item>
-        </Col>
-
-        <Form.Item
-          name="radio-group"
-          label="Do you need funding from the USDA?"
-          className="label"
-        >
-          <Radio.Group>
-            <Radio style={radioStyle} value="a">
-              Yes
-            </Radio>
-            <Radio style={radioStyle} value="b">
-              No
-            </Radio>
-          </Radio.Group>
-        </Form.Item>
-        <Form.Item>
-          <Button
-            type="primary"
-            className="aquaponic-button-submit"
-            htmlType="submit"
-            disabled={false}
-          >
-            Generate report
-          </Button>
-        </Form.Item>
-      </Form>
-    </div>
+    <form name="contact" netlify>
+      <p>
+        <label>Name <input type="text" name="name" /></label>
+      </p>
+      <p>
+        <label>Email <input type="email" name="email" /></label>
+      </p>
+      <p>
+        <button type="submit">Send</button>
+      </p>
+    </form>
   );
 };
 
